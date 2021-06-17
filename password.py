d1660a29e36856195f3090905fe106d38f6a49f7
Vanessa Liu <vanessaliu926@gmail.com>
2021-06-17 18:32:29 +0800

    finished version done

--- password.py
+++ password.py
@@ -1,10 +1,12 @@
 password = 'a123456'
 i = 3
 while i > 0:
-    user = input('請輸入密碼: ') 
-    if user == password:
+    i = i - 1
+    pwd = input('請輸入密碼:')
+    if pwd == password:
         print('登入成功')
         break 
     else:
-        i = i -1
-        print('密碼錯誤！還有', number, '次機會')
+        print('密碼錯誤!')
+        if i > 0: # 剩餘0次的時候不要印剩下幾次機會
+            print('還有', i ,'次機會')
